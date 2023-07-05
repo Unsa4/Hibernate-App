f44765f454410113cc89a6334f17ef1e435acc32
Unsa Fahim <94924307+Unsa4@users.noreply.github.com>
2023-07-05 15:37:35 +0500

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,44 @@
+package entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(name = "FNAME")
+    private String fName;
+    @Column(name = "LNAME")
+    private String lName;
+
+    public Employee() {
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+}
